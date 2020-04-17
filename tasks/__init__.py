31ba11a8f84184ecd68c63a9c8455d3b77db2a29
Jonatan Männchen <jonatan@maennchen.ch>
2020-04-17 13:45:14 +0100

    Improve Build Times for Development

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -32,20 +32,23 @@ def build(ctx, release=False, compress=False, version=VERSION, gcsfuse=GCSFUSE_V
     if release:
         static_image = IMAGE_LATEST
         global_ldflags = '-s -w'
+        docker_build_args = '--no-cache'
     else:
         static_image = IMAGE_DEV
         global_ldflags = ''
         version += '-rc'
+        docker_build_args = ''
 
     upx_flags = '--best --ultra-brute' if compress else ''
     image = image_name(version)
 
     ctx.run(
-        f'docker build . --no-cache --tag {image} '
+        f'docker build . --tag {image} '
         f'--build-arg version={version} '
         f'--build-arg global_ldflags="{global_ldflags}" '
         f'--build-arg gcsfuse_version="{gcsfuse}" '
-        f'--build-arg upx_flags="{upx_flags}"',
+        f'--build-arg upx_flags="{upx_flags}" '
+        f'{docker_build_args}',
         echo=True,
     )
 
