30bd0cf34aebf311f3daf2d0bd347e4b541881d2
Ofek Lev <ofekmeister@gmail.com>
2020-04-20 12:53:26 -0400

    fix docs publishing

--- tasks/utils.py
+++ tasks/utils.py
@@ -10,24 +10,6 @@ def get_latest_commit_hash(ctx):
     return result.stdout.strip()
 
 
-def get_git_user(ctx):
-    user = os.getenv('GH_USER')
-    if user is not None:
-        return user
-
-    result = ctx.run('git config --get user.name', hide=True)
-    return result.stdout.strip()
-
-
-def get_git_email(ctx):
-    email = os.getenv('GH_EMAIL')
-    if email is not None:
-        return email
-
-    result = ctx.run('git config --get user.email', hide=True)
-    return result.stdout.strip()
-
-
 def create_file(f):
     with open(f, 'a'):
         os.utime(f, None)
