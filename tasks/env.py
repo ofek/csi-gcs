8deb5a279965909f51680269e29e7e58b18cdd9f
Ofek Lev <ofekmeister@gmail.com>
2020-04-24 18:21:05 -0400

    refactor env command

--- tasks/env.py
+++ tasks/env.py
@@ -1,41 +1,52 @@
-import os
-from invoke import Exit, task
+from invoke import task
 
 from .constants import DRIVER_NAME, GCSFUSE_VERSION
+from .utils import get_root
+
 
 @task
-def build_environment(ctx):
+def create(ctx):
     ctx.run(
-        f'docker volume create csi-gcs-go',
+        'docker volume create csi-gcs-go',
         echo=True,
     )
     ctx.run(
         f'docker build . '
-        f'--tag {DRIVER_NAME}/env '
+        f'--tag {DRIVER_NAME}-env '
         f'-f dev-env.Dockerfile '
         f'--build-arg gcsfuse_version="{GCSFUSE_VERSION}"',
         echo=True,
     )
 
+
+@task
+def delete(ctx):
+    ctx.run(
+        'docker volume rm csi-gcs-go',
+        echo=True,
+    )
+    ctx.run(
+        f'docker image rm {DRIVER_NAME}-env',
+        echo=True,
+    )
+
+
 @task(
-    pre=[build_environment],
+    pre=[create],
     default=True,
 )
 def run(ctx, command='echo Provide Command'):
-    pwd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
     ctx.run(
         f'docker run '
         f'--rm '
-        f'-v {pwd}:/driver '
+        f'-v {get_root()}:/driver '
         f'-v csi-gcs-go:/go '
-        f'-p 8765:8765 '
         f'--cap-add SYS_ADMIN '
         f'--device /dev/fuse '
         f'--privileged '
         f'-v /tmp/csi:/tmp/csi:rw '
         f'-v /var/run/docker.sock:/var/run/docker.sock '
-        f'{DRIVER_NAME}/env '
+        f'{DRIVER_NAME}-env '
         f'{command}',
         echo=True,
     )
