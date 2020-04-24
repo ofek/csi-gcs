045e1bdadf388042c5f5a727210c223f407867df
Ofek Lev <ofekmeister@gmail.com>
2020-04-24 18:08:08 -0400

    rename

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,7 +3,7 @@ from invoke import Collection
 from . import docs
 from . import image
 from . import test
-from . import docker
+from . import env
 from . import build
 from .utils import set_root
 
@@ -11,7 +11,7 @@ ns = Collection()
 ns.add_collection(Collection.from_module(image))
 ns.add_collection(Collection.from_module(docs))
 ns.add_collection(Collection.from_module(test))
-ns.add_collection(Collection.from_module(docker))
+ns.add_collection(Collection.from_module(env))
 ns.add_collection(Collection.from_module(build))
 
 set_root()
