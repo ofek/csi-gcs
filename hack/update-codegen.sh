200ebf1a688074317a01e2b9172267589265b145
Ofek Lev <ofekmeister@gmail.com>
2022-05-14 10:06:14 -0400

    Update update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ash
 
 # docker run --rm -v $PWD:/go/src/github.com/ofek/csi-gcs -w /go/src/github.com/ofek/csi-gcs golang:1.18.2-alpine3.15 ./hack/update-codegen.sh
-apk add --update --no-cache fuse fuse-dev git bash
+apk add --update --no-cache bash
 go get -u k8s.io/code-generator@v0.24.0
 
 CLIENTSET_NAME_VERSIONED=clientset \
